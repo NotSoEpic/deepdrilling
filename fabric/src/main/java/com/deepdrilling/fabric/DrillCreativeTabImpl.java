ae088bfa064f5731a3b1814f161335692f4943f8
NotSoEpic <legotomsmith@gmail.com>
2024-07-15 00:18:22 +0800

    Forgor about translation

--- fabric/src/main/java/com/deepdrilling/fabric/DrillCreativeTabImpl.java
+++ fabric/src/main/java/com/deepdrilling/fabric/DrillCreativeTabImpl.java
@@ -3,18 +3,18 @@ package com.deepdrilling.fabric;
 import com.deepdrilling.DBlocks;
 import com.deepdrilling.DrillCreativeTab;
 import com.deepdrilling.DrillMod;
+import com.simibubi.create.foundation.utility.Components;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 
 public class DrillCreativeTabImpl {
     public static final ResourceKey<CreativeModeTab> KEY = ResourceKey.create(Registries.CREATIVE_MODE_TAB, DrillMod.id("main"));
     private static final CreativeModeTab TAB = registerTab(FabricItemGroup.builder()
-            .title(Component.literal("Deep Drilling"))
+            .title(Components.translatable("itemGroup.deepdrilling.main"))
             .icon(() -> DBlocks.DRILL.asItem().getDefaultInstance())
             .displayItems(new DrillCreativeTab.ItemDisplay())
             .build());
